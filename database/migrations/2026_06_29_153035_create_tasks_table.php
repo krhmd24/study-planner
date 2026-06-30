2ef489bd375c73d96953cb7edb54e6e58435d82e
krhmd24 <rhmtkrnwan14@gmail.com>
2026-06-30 11:23:04 +0700

    Create Eloquent models and relationships

--- database/migrations/2026_06_29_153035_create_tasks_table.php
+++ database/migrations/2026_06_29_153035_create_tasks_table.php
@@ -9,14 +9,26 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up(): void
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
 
+        $table->foreignId('course_id')
+              ->constrained()
+              ->onDelete('cascade');
+
+        $table->string('title');
+
+        $table->text('description')->nullable();
+
+        $table->date('deadline');
+
+        $table->boolean('status')->default(false);
+
+        $table->timestamps();
+    });
+}
     /**
      * Reverse the migrations.
      */
